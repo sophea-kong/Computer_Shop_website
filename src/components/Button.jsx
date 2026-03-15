c5955d77986baa6a2700a2c16b5f920b0a068b8b
Sophea Kong <sopheakongs001@gmail.com>
2026-03-15 13:58:18 +0700

    button and nav-bar

---
+++ src/components/Button.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { ShoppingCart } from "lucide-react";
+
+function Button({ text,color,holdcolor }) {
+    let c = "flex items-center px-4 py-2 rounded-xl bg-[" + color + "] text-white hover:bg-["+ holdcolor +"]"
+    if(color=="#ffffff"){
+        c = "flex items-center px-4 py-2 rounded-xl bg-[" + color + "] text-black hover:bg-["+ holdcolor +"]"
+    }
+    if (holdcolor=="#ffffff"){
+        c += " hover:text-black";
+    }
+    return (
+    <button className={c}>{text}</button>
+    );
+}
+
+export { Button };
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,25 @@
+import { Button } from "./Button";
+import {Cpu,ShoppingCart} from 'lucide-react'
+
+function NavBar(){
+    return (
+        <> 
+        <div className="flex pl-10 pt-5 pr-10 pb-5 border-0 border-b-1 border border-[#c9def4] border-opacity-5">
+            <div id="logo" className="flex w-60 items-center">
+                <Cpu className="bg-[#296eb4] p-2 rounded-xl" color="#ffffff" size={50}></Cpu>
+                <p className="pl-3 text-md font-bold">PC BUILDER SHOP</p>
+            </div>
+            <div id="blank" className="w-[60%]"></div>
+            <div className="flex gap-4 font-semibold">
+                <Button text="Home" color="#ffffff" holdcolor="#d6d6d6" />
+                <Button text="Contact" color="#ffffff" holdcolor="#d6d6d6" />
+                <Button text="Build" color="#ffffff" holdcolor="#d6d6d6" />
+                <Button text={<><ShoppingCart></ShoppingCart><p>Build</p></>} color="#296eb4" holdcolor="#296eb4"/>
+            </div>
+        </div>
+        
+        </>
+    )
+}
+
+export default NavBar;
