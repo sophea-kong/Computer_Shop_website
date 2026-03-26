13208331a600d33540b96f36f659327b32422f6e
VorngViseth <vorngviseth077@gmail.com>
2026-03-26 13:04:25 +0700

    make it look pretty

--- src/pages/Build.jsx
+++ src/pages/Build.jsx
@@ -47,6 +47,10 @@ export default function Build() {
     const [inStockCases , setInStockCases] = useState(CASES);
     const [inStockCooler , setInStockCooler] = useState(COOLERS);
 
+    // ------------------------------
+    // helper functions
+    // -----------------------------
+
     function handleBrandChange(value, isChecked) {
         setBrand((prevBrand) =>
             isChecked ? [...prevBrand, value] : prevBrand.filter((b) => b !== value)
@@ -85,7 +89,6 @@ export default function Build() {
     }
 
     const selectedComponentsCount = countComponentsInBuild(); ;
-
     function countComponentsInBuild() {
         let count = 0;
         for (const category in selectedBuild) {
@@ -101,169 +104,205 @@ export default function Build() {
         setSelectedBuild(INITIAL_SELECTED_BUILD);
     }
 
-    return (
-    <>
+    // ------------------------------
+    // React Component functions
+    // -----------------------------
+    function BuildAndOrder() {
+        return (
+            <div className="flex flex-col justify-center px-10 py-15 gap-5 text-black bg-gradient-to-br from-white from-50% to-[#296eb4] border-y-1 border-gray-300">
+                <h1>Build & Order</h1>
+                <p className="!text-[var(--text-muted)] !text-[25px]">Choose your components and create your custom PC build</p>
+            </div>
+        )
+    }
 
-        <Navbar></Navbar>
+    function Search() {
+        return (
+            <search className="bg-gray-100 rounded-lg p-3">
+                <form
+                    action=""
+                    className="w-full flex flex-row gap-2"
+                    onSubmit={(event) => event.preventDefault()}
+                >
+                    <input
+                        type="text"
+                        placeholder="Search parts..."
+                        className="w-full outline-none border-none"
+                        value={searchQuery}
+                        onChange={(event) => setSearchQuery(event.target.value)}
+                    />
+                    <button type="submit" className="flex items-center gap-1">
+                        <span className="material-symbols-outlined text-[20px]">search</span>
+                        Search
+                    </button>
+                </form>
+            </search>
+        )
+    }
 
-        {/* <Button text="Back to Home" color="#296eb4" holdcolor="#296eb4" /> */}
-        <div className="flex flex-col justify-center px-10 py-15 gap-5 text-black bg-gradient-to-br from-white from-50% to-[#296eb4] border-y-1 border-gray-300">
-            <h1>Build & Order</h1>
-            <p className="!text-[var(--text-muted)] !text-[25px]">Choose your components and create your custom PC build</p>
+    function FilterAndCategory() {
+        return (
+            <div className="flex flex-col border-1 border-gray-300 rounded-[15px] p-5 gap-3">
+                {/* price range slider */}
+                <div className="flex flex-col gap-2">
+                    <p>Price Range : 0$ - {priceRange}$</p>
+                    <div>
+                        <div className="flex flex-row justify-between !text-[var(--text-muted)]">
+                            <p>0$</p>
+                            <p>2000$</p>
+                        </div>
+                        <input type="range" min="0" max="2000" 
+                            className="w-full"
+                            value={priceRange} 
+                            onChange={(event) => setPriceRange(event.target.value)} 
+                        />
+                    </div>
+                </div>
+
+                {/* brand */}
+                <div className="flex flex-col gap-2">
+                    <p>Brand</p>
+                    <label className="flex items-center gap-2">
+                        <input
+                            type="checkbox"
+                            name="intel"
+                            value="intel"
+                            checked={brand.includes("intel")}
+                            onChange={(event) => handleBrandChange("intel", event.target.checked)}
+                        />
+                        Intel
+                    </label>
+                    <label className="flex items-center gap-2">
+                        <input
+                            type="checkbox"
+                            name="amd"
+                            value="amd"
+                            checked={brand.includes("amd")}
+                            onChange={(event) => handleBrandChange("amd", event.target.checked)}
+                        />
+                        AMD
+                    </label>
+
+                    {/* <p>brand you choose : {brand.join(", ")}</p> */}
+                </div>
+
+                {/* sort by */}
+                <div className="flex flex-col gap-2">
+                    <p>Sort by</p>
+                    <nav>
+                        <ul className="flex flex-col gap-2">
+                            <li><button className={getSortByButtonClass("price-low-to-high")} onClick={() => setSortBy("price-low-to-high")}>Price: Low to High</button></li>
+                            <li><button className={getSortByButtonClass("price-high-to-low")} onClick={() => setSortBy("price-high-to-low")}>Price: High to Low</button></li>
+                            <li><button className={getSortByButtonClass("highest-rating")} onClick={() => setSortBy("highest-rating")}>Highest Rating</button></li>
+                            <li><button className={getSortByButtonClass("name-az")} onClick={() => setSortBy("name-az")}>Name (A-Z)</button></li>
+                        </ul>
+                        {/* <p>sort selected: {sortBy}</p> */}
+                    </nav>
+                </div>
+            </div>
+        )
+    }
+
+    function SearchAndFilter() {
+        return (
+        <div className="flex flex-col gap-8">
+            {/* search parts */}
+            <Search />
+
+            {/* filter and the selected category*/}
+            <FilterAndCategory />
         </div>
+        )
+    }
 
+    function CategorySelecion() {
+        return (
+        <nav>
+            <ul className="flex gap-5">
+                <li><button className={getCategoryButtonClass("CPU")} onClick={() => setSelectedCategory("CPU")}>CPU</button></li>
+                <li><button className={getCategoryButtonClass("GPU")} onClick={() => setSelectedCategory("GPU")}>GPU</button></li>
+                <li><button className={getCategoryButtonClass("RAM")} onClick={() => setSelectedCategory("RAM")}>RAM</button></li>
+                <li><button className={getCategoryButtonClass("Storage")} onClick={() => setSelectedCategory("Storage")}>Storage</button></li>
+                <li><button className={getCategoryButtonClass("Motherboard")} onClick={() => setSelectedCategory("Motherboard")}>Motherboard</button></li>
+                <li><button className={getCategoryButtonClass("PSU")} onClick={() => setSelectedCategory("PSU")}>PSU</button></li>
+                <li><button className={getCategoryButtonClass("Cases")} onClick={() => setSelectedCategory("Cases")}>Cases</button></li>
+                <li><button className={getCategoryButtonClass("Cooler")} onClick={() => setSelectedCategory("Cooler")}>Cooler</button></li>
+            </ul>
+        {/* <p>what you choose : {selectedBuild}</p> */}
+        </nav>
+        )
+    }
+
+    function FilterAndBuild() {
+        return (
         <div className="grid grid-cols-3 gap-10 p-10 bg-[#fbfef9]">
             <div className="col-span-2 gap-8 flex flex-col">
                 {/* Search parts and Price Range grid and the component selection */}
-                <div className="flex flex-col gap-8">
-                    {/* search parts */}
-                    <search className="bg-gray-100 rounded-lg p-3">
-                        <form
-                            action=""
-                            className="w-full flex flex-row gap-2"
-                            onSubmit={(event) => event.preventDefault()}
-                        >
-                            <input
-                                type="text"
-                                placeholder="Search parts..."
-                                className="w-full outline-none border-none"
-                                value={searchQuery}
-                                onChange={(event) => setSearchQuery(event.target.value)}
-                            />
-                            <button type="submit" className="flex items-center gap-1">
-                                <span className="material-symbols-outlined text-[20px]">search</span>
-                                Search
-                            </button>
-                        </form>
-                    </search>
-
-                    <div className="flex flex-col border-1 border-gray-300 rounded-[15px] p-5 gap-3">
-                        {/* price range slider */}
-                        <div className="flex flex-col gap-2">
-                            <p>Price Range : 0$ - {priceRange}$</p>
-                            <div>
-                                <div className="flex flex-row justify-between !text-[var(--text-muted)]">
-                                    <p>0$</p>
-                                    <p>2000$</p>
-                                </div>
-                                <input type="range" min="0" max="2000" 
-                                    className="w-full"
-                                    value={priceRange} 
-                                    onChange={(event) => setPriceRange(event.target.value)} 
-                                />
-                            </div>
-                        </div>
-
-                        {/* brand */}
-                        <div className="flex flex-col gap-2">
-                            <p>Brand</p>
-                            <label className="flex items-center gap-2">
-                                <input
-                                    type="checkbox"
-                                    name="intel"
-                                    value="intel"
-                                    checked={brand.includes("intel")}
-                                    onChange={(event) => handleBrandChange("intel", event.target.checked)}
-                                />
-                                Intel
-                            </label>
-                            <label className="flex items-center gap-2">
-                                <input
-                                    type="checkbox"
-                                    name="amd"
-                                    value="amd"
-                                    checked={brand.includes("amd")}
-                                    onChange={(event) => handleBrandChange("amd", event.target.checked)}
-                                />
-                                AMD
-                            </label>
-
-                            {/* <p>brand you choose : {brand.join(", ")}</p> */}
-                        </div>
-
-                        {/* sort by */}
-                        <div className="flex flex-col gap-2">
-                            <p>Sort by</p>
-                            <nav>
-                                <ul className="flex flex-col gap-2">
-                                    <li><button className={getSortByButtonClass("price-low-to-high")} onClick={() => setSortBy("price-low-to-high")}>Price: Low to High</button></li>
-                                    <li><button className={getSortByButtonClass("price-high-to-low")} onClick={() => setSortBy("price-high-to-low")}>Price: High to Low</button></li>
-                                    <li><button className={getSortByButtonClass("highest-rating")} onClick={() => setSortBy("highest-rating")}>Highest Rating</button></li>
-                                    <li><button className={getSortByButtonClass("name-az")} onClick={() => setSortBy("name-az")}>Name (A-Z)</button></li>
-                                </ul>
-                                {/* <p>sort selected: {sortBy}</p> */}
-                            </nav>
-                        </div>
-                    </div>
-                </div>
+                <SearchAndFilter />
             
                 {/* category selection */}
-                <nav>
-                    <ul className="flex gap-5">
-                        <li><button className={getCategoryButtonClass("CPU")} onClick={() => setSelectedCategory("CPU")}>CPU</button></li>
-                        <li><button className={getCategoryButtonClass("GPU")} onClick={() => setSelectedCategory("GPU")}>GPU</button></li>
-                        <li><button className={getCategoryButtonClass("RAM")} onClick={() => setSelectedCategory("RAM")}>RAM</button></li>
-                        <li><button className={getCategoryButtonClass("Storage")} onClick={() => setSelectedCategory("Storage")}>Storage</button></li>
-                        <li><button className={getCategoryButtonClass("Motherboard")} onClick={() => setSelectedCategory("Motherboard")}>Motherboard</button></li>
-                        <li><button className={getCategoryButtonClass("PSU")} onClick={() => setSelectedCategory("PSU")}>PSU</button></li>
-                        <li><button className={getCategoryButtonClass("Cases")} onClick={() => setSelectedCategory("Cases")}>Cases</button></li>
-                        <li><button className={getCategoryButtonClass("Cooler")} onClick={() => setSelectedCategory("Cooler")}>Cooler</button></li>
-                    </ul>
-                {/* <p>what you choose : {selectedBuild}</p> */}
-                </nav>
+                <CategorySelecion />
             </div>
 
             {/* Start your build */}
-            <div className="col-span-1 h-[650px]">
-                {/* hide if the user has selected at least one component */}
-                {selectedComponentsCount === 0 && (
-                    <div className="flex flex-col gap-5 items-center justify-center border-1 border-gray-300 rounded-[15px] px-10 py-20">
-                        
-                        <h1>Start Your Build</h1>
-                        <p className="text-center !text-[var(--text-muted)]">
-                            No parts selected yet. Begin by choosing a CPU to start building your perfect PC.
-                        </p>
-                        <button className="flex py-2 px-4 border-1 bg-[#296eb4] !text-white rounded-lg gap-2 items-center" 
-                            onClick={() => setSelectedCategory("CPU")}>
-                            Browse CPUs
-                            <span className="material-symbols-outlined !text-white">
-                                arrow_forward
-                            </span>
-                        </button>
-                    </div>
-                )}
+            <StartYourBuild />
+        </div>
+        )
+    }
 
-                {/* show the selected components */}
-                {selectedComponentsCount > 0 && (
-                    <div className="h-full overflow-y-auto border-1 border-gray-300 rounded-[15px] p-5">
-                        <h1>Selected Components</h1>
-                        {BUILD_CATEGORIES.map((category) =>
-                            renderSelectedComponentCard({
-                                category,
-                                component: selectedBuild[category],
-                                onBrowseCategory: setSelectedCategory,
-                                onRemove: handleRemoveFromBuild,
-                            })
-                        )}
-                        {selectedComponentsCount === BUILD_CATEGORIES.length && (
-                            <div>
-                                <button className="w-full flex items-center justify-center py-3 bg-[#296eb4] !text-white rounded-lg mt-5 gap-2"
-                                    onClick={addToCart}>
-                                    <span className="material-symbols-outlined !text-white">
-                                        shopping_cart
-                                    </span>
-                                    Add Build to Cart
-                                </button>
-                            </div>
-                        )}
-                    </div>
-                )}
+    function StartYourBuild() {
+        return (
+        <div className="col-span-1 h-[650px]">
+            {/* hide if the user has selected at least one component */}
+            {selectedComponentsCount === 0 && (
+                <div className="flex flex-col gap-5 items-center justify-center border-1 border-gray-300 rounded-[15px] px-10 py-20">
+                    
+                    <h1>Start Your Build</h1>
+                    <p className="text-center !text-[var(--text-muted)]">
+                        No parts selected yet. Begin by choosing a CPU to start building your perfect PC.
+                    </p>
+                    <button className="flex py-2 px-4 border-1 bg-[#296eb4] !text-white rounded-lg gap-2 items-center" 
+                        onClick={() => setSelectedCategory("CPU")}>
+                        Browse CPUs
+                        <span className="material-symbols-outlined !text-white">
+                            arrow_forward
+                        </span>
+                    </button>
+                </div>
+            )}
 
-            </div>            
+            {/* show the selected components */}
+            {selectedComponentsCount > 0 && (
+                <div className="h-full overflow-y-auto border-1 border-gray-300 rounded-[15px] p-5">
+                    <h1>Selected Components</h1>
+                    {BUILD_CATEGORIES.map((category) =>
+                        renderSelectedComponentCard({
+                            category,
+                            component: selectedBuild[category],
+                            onBrowseCategory: setSelectedCategory,
+                            onRemove: handleRemoveFromBuild,
+                        })
+                    )}
+                    {selectedComponentsCount === BUILD_CATEGORIES.length && (
+                        <div>
+                            <button className="w-full flex items-center justify-center py-3 bg-[#296eb4] !text-white rounded-lg mt-5 gap-2"
+                                onClick={addToCart}>
+                                <span className="material-symbols-outlined !text-white">
+                                    shopping_cart
+                                </span>
+                                Add Build to Cart
+                            </button>
+                        </div>
+                    )}
+                </div>
+            )}
 
         </div>
+        )
+    }
 
-        {/* card for different components and the price and the add to build button */}
+    function Products() {
+        return (
         <div className="mb-10">
             {selectedCategory === "CPU" && (
                 cardRenderer("CPU", inStockCpu, priceRange, brand, handleAddToBuild, selectedBuild, sortBy, searchQuery)
@@ -290,6 +329,21 @@ export default function Build() {
                 cardRenderer("Cooler", inStockCooler, priceRange, brand, handleAddToBuild, selectedBuild, sortBy, searchQuery)
             )}
         </div>
+        )
+    }
+
+    return (
+    <>
+        <Navbar></Navbar>
+
+        {/* <Button text="Back to Home" color="#296eb4" holdcolor="#296eb4" /> */}
+        <BuildAndOrder />
+        
+        {/* Body */}
+        <FilterAndBuild />
+
+        {/* card for different components and the price and the add to build button */}
+        <Products />
 
         <Footer></Footer>
     </>
